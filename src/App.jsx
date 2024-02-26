a68d09647d0fe5d0979e6631e0b2c3339c69fd7e
Aman-Rautela <159975822+Aman-Rautela@users.noreply.github.com>
2024-02-26 23:40:06 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Content from './assets/Component/Content';
+import Navigation from './assets/Component/Navigation';
+const App =()=>{
+  return(
+    <div>
+      <Navigation />
+      <Content />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/Component/Content.jsx
@@ -0,0 +1,30 @@
+const Content = () => {
+return(
+    <main className="cont  container">
+        <div className="content">
+            <h1>
+                YOUR FEET DESERVE THE BEST
+            </h1>
+            <p>
+                YOUR FEET DESERV THE BEST AND WE'RE HERE TO HEPL YOU WITH OUR SHOES.
+                YOUR FEET DESERV THE BEST AND WE'RE HERE TO HEPL YOU WITH OUR SHOES. 
+            </p>
+        
+        <div className="shop-button">
+            <button>Shop Now</button>
+            <button className="secondary-btn">Category</button>
+        </div>
+        <div className="shopping">
+            <p>Also Available On</p>
+            <div className="brand-icons">
+                <img src="/images/shops.png" alt="logo" />            </div>
+        </div>
+        </div>
+        <div className="logo">
+            <img className="logo" src="/images/shoe_image.png" alt="logo" />
+        </div>
+    </main>
+);
+}
+
+export default Content;
